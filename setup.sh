e09807156e375736eecb69cec18bfb9abbe1094e
alonm16 <45255645+alonm16@users.noreply.github.com>
2022-03-09 21:37:02 +0200

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+conda env update -f environment.yml
+conda activate zeroshot
+conda install --yes -c pytorch pytorch=1.7.1 torchvision cudatoolkit=11.0
+pip install ftfy regex tqdm
+pip install git+https://github.com/openai/CLIP.git
