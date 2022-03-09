1a13ab0f474cbc0842b03cd2a2978887fb072896
Alon Mor <STAFF\almr16@tdk-bm1.csf.technion.ac.il>
2022-03-09 20:00:22 +0000

    change env

--- setup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-conda env update -f environment.yml
-conda activate zeroshot
-conda install --yes -c pytorch pytorch=1.7.1 torchvision cudatoolkit=11.0
-pip install ftfy regex tqdm
-pip install git+https://github.com/openai/CLIP.git
-conda deactivate
